2c5fefadf303f0c1636a5be090fce6c3aadb6dd2
Ramesh <vinaykorada2004@gmail.com>
2025-05-29 11:53:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello There!</h1>
+      <h3>This is my first React app, that i deployed on GitHub pages</h3>
+    </div>
+  )
+}
+
+export default App
